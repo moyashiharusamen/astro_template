cf9b85387f52f23c04d0216c5f108afa70c6b745
moyashiharusamen <moyashi.harusamen@gmail.com>
2023-01-27 15:20:05 +0900

    eslint を導入
    - .astro ファイルを対象とすると、 parse エラーが発生するため、一旦対象から除外

---
+++  .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+    },
+    parserOptions: {
+        sourceType: "module"
+    },
+    extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:astro/recommended",
+    ],
+    plugins: ["@typescript-eslint"],
+    overrides: [
+        {
+            files: ["*.astro"],
+            parser: "astro-eslint-parser",
+            parserOptions: {
+                parser: "@typescript-eslint/parser",
+                extraFileExtensions: [".astro"],
+            },
+        }
+    ]
+};
---
+++ src/script/main.js
@@ -0,0 +1,6 @@
+export default class Hoge {
+    constructor() {
+        const header = document.querySelector('header');
+        console.log(header);
+    }
+}
