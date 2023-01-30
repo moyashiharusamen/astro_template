a6fdcff46b9785ab0dc5d947e7e0559676914de6
moyashiharusamen <moyashi.harusamen@gmail.com>
2023-01-31 00:33:01 +0900

    型を設定

--- src/script/main.ts
+++ src/script/main.ts
@@ -1,6 +1,6 @@
 export default class BaseLayout {
     constructor() {
-        const header = document.querySelector('body');
+        const header: HTMLBodyElement = <HTMLBodyElement>document.querySelector('body');
         header.classList.add('BaseLayout');
     }
 }
