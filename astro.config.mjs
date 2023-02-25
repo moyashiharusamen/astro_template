764eb37b1b5714e5499e6e23c12851cb23c20905
moyashiharusamen <moyashi.harusamen@gmail.com>
2023-02-26 00:03:34 +0900

    js ファイルも minify するように設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ export default defineConfig({
             css: true,
             html: true,
             img: false,
-            js: false,
+            js: true,
             svg: false,
         }),
     ],
