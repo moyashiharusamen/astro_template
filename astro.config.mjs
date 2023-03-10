b42832a4a4dcc32128197b16d1dc2167e188a594
moyashiharusamen <moyashi.harusamen@gmail.com>
2023-03-10 22:37:37 +0900

    css ファイルの build 時の命名規則を変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
         build: {
             rollupOptions: {
                 output: {
-                    assetFileNames: 'assets/style/style.[hash][extname]',
+                    assetFileNames: 'assets/style/[name].[extname]',
                 },
             },
         },
