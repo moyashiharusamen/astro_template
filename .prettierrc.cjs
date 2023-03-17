294406c53f6579bcec5044f0e687b69e1e5b4265
moyashiharusamen <moyashi.harusamen@gmail.com>
2023-03-17 19:06:56 +0900

    refactor: インデントを調整

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,25 +1,25 @@
 module.exports = {
-    plugins: [require.resolve('prettier-plugin-astro')],
-    overrides: [
-        {
-            files: '*.astro, *.scss, *.js, *.ts, *.vue',
-            options: {
-                parser: 'astro',
-            },
-        },
-    ],
-    // 行末にセミコロン
-    semi: true,
-    // 末尾のカンマ
-    trailingComma: "none",
-    // アロー関数の引数のカッコを可能な限り省略する
-    arrowParens: "avoid",
-    // 長い行の折り返し位置
-    printWidth: 100,
-    // インデントのサイズ
-    tabWidth: 2,
-    // 改行コードの指定
-    endOfLine: "lf",
-    // 属性の改行
-    singleAttributePerLine: true,
+  plugins: [require.resolve('prettier-plugin-astro')],
+  overrides: [
+    {
+      files: '*.astro, *.scss, *.js, *.ts, *.vue',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+  // 行末にセミコロン
+  semi: true,
+  // 末尾のカンマ
+  trailingComma: "none",
+  // アロー関数の引数のカッコを可能な限り省略する
+  arrowParens: "avoid",
+  // 長い行の折り返し位置
+  printWidth: 100,
+  // インデントのサイズ
+  tabWidth: 2,
+  // 改行コードの指定
+  endOfLine: "lf",
+  // 属性の改行
+  singleAttributePerLine: true,
 };
