258ab500ae27abc1fb70a7f99928256a96114b25
moyashiharusamen <moyashi.harusamen@gmail.com>
2023-04-15 21:28:44 +0900

    refactor: インデントの調整

---  .eslintrc.js
+++  .eslintrc.js
@@ -1,25 +1,22 @@
 module.exports = {
-    root: true,
-    env: {
-        browser: true,
-        node: true,
-    },
-    parserOptions: {
-        sourceType: "module"
-    },
-    extends: [
-        "plugin:@typescript-eslint/recommended",
-        "plugin:astro/recommended",
-    ],
-    plugins: ["@typescript-eslint"],
-    overrides: [
-        {
-            files: ["*.astro"],
-            parser: "astro-eslint-parser",
-            parserOptions: {
-                parser: "@typescript-eslint/parser",
-                extraFileExtensions: [".astro"],
-            },
-        }
-    ]
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    sourceType: "module"
+  },
+  extends: ["plugin:@typescript-eslint/recommended", "plugin:astro/recommended"],
+  plugins: ["@typescript-eslint"],
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"]
+      }
+    }
+  ]
 };
