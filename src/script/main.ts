da2a4ff9e52c98e2e5fafe05f9f927d1665103d7
moyashiharusamen <moyashi.harusamen@gmail.com>
2023-06-21 17:05:50 +0900

    feat: テキストの色が遷移するアニメーション機能を作成

--- src/script/main.ts
+++ src/script/main.ts
@@ -2,8 +2,10 @@ import Toggle from './Toggle';
 import Modal from './Modal';
 import Accordion from './Accordion';
 import Tab from './Tab';
+import TextTransition from './TextTransition';
 
 Toggle.createAll();
 Modal.createAll();
 Accordion.createAll();
 Tab.createAll();
+TextTransition.createAll();
