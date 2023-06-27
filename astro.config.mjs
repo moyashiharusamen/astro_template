891bb271f924a56b2f44ab0383432cd8acbeba78
moyashiharusamen <moyashi.harusamen@gmail.com>
2023-06-27 19:18:56 +0900

    feat: 設定を変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,7 +15,6 @@ export default defineConfig({
         output: {
           assetFileNames: 'assets/style/[name].[extname]',
         },
-        external: ['uuidv4', 'lodash'],
       },
     },
   },
